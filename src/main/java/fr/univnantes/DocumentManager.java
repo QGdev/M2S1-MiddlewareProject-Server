a271534ee20b39d02ce766688b8d82df8d9365e8
Quentin GOMES DOS REIS <quentin.gomes.dev@gmail.com>
2023-10-31 14:46:00 +0100

    Creation of DocumentManager used to manage documents

---
+++ src/main/java/fr/univnantes/DocumentManager.java
@@ -0,0 +1,75 @@
+package fr.univnantes;
+
+import fr.univnantes.document.Document;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Class used to manage documents
+ * <p>
+ *     This class is used to create, get and remove documents
+ *     It is a singleton
+ * </p>
+ */
+public class DocumentManager {
+
+    private final static AtomicReference<DocumentManager> instance = new AtomicReference<>(null);
+    private final ConcurrentHashMap<String, Document> documents;
+
+
+    /**
+     * Creates a new document manager
+     */
+    private DocumentManager() {
+        documents = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * Returns the instance of the document manager
+     * Creates it if it does not exist
+     *
+     * @return  The instance of the document manager
+     */
+    public static DocumentManager getInstance() {
+        if (instance.get() == null) {
+            synchronized (DocumentManager.class) {
+                instance.compareAndSet(null, new DocumentManager());
+            }
+        }
+        return instance.get();
+    }
+
+    /**
+     * Returns the document with the given UUID
+     *
+     * @param documentId    The UUID of the document
+     * @return              The document with the given UUID
+     */
+    public Document getDocument(String documentId) {
+        return documents.get(documentId);
+    }
+
+    /**
+     * Creates a new document with the given name
+     *
+     * @param name  The name of the document
+     * @return      The created document
+     */
+    public Document createDocument(String name) {
+        Document document = new Document(name);
+        documents.put(document.getUUID(), document);
+        return document;
+    }
+
+    /**
+     * Removes the document with the given UUID
+     *
+     * @param documentId    The UUID of the document
+     * @return              True if the document was removed, false otherwise
+     */
+    public boolean removeDocument(String documentId) {
+        return documents.remove(documentId) != null;
+    }
+
+}
