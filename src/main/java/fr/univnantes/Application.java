04a1fffa25614a4417ce8a04fb80b74f1ca36cb5
Quentin GOMES DOS REIS <quentin.gomes.dev@gmail.com>
2023-10-31 14:48:01 +0100

    Added Application.java used to start the app.

---
+++ src/main/java/fr/univnantes/Application.java
@@ -0,0 +1,26 @@
+package fr.univnantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class of the application.
+ * <p>
+ *     This class is used to start the application.
+ *     It is also used to initialize the document manager and the user manager.
+ * </p>
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Main method of the application.
+     * @param args  The arguments of the application passed by the command line (only used for SpringBoot)
+     */
+    public static void main(String[] args) {
+        DocumentManager documentManager = DocumentManager.getInstance();
+        UserManager userManager = UserManager.getInstance();
+
+        SpringApplication.run(Application.class, args);
+    }
+}
