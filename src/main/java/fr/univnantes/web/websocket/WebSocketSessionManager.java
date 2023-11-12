e922aef7716c16103d554a97a2e5f30cf6c993ab
Quentin GOMES DOS REIS <quentin.gomes.dev@gmail.com>
2023-11-12 14:26:06 +0100

    Added websocket session to user map in WebSocketSessionManager.java

--- src/main/java/fr/univnantes/web/websocket/WebSocketSessionManager.java
+++ src/main/java/fr/univnantes/web/websocket/WebSocketSessionManager.java
@@ -1,5 +1,6 @@
 package fr.univnantes.web.websocket;
 
+import fr.univnantes.user.User;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,14 +15,16 @@ public class WebSocketSessionManager {
 
     private static final  AtomicReference<WebSocketSessionManager> instance = new AtomicReference<>(null);
     private final ConcurrentHashMap<WebSocketSession, String> documentsSessions;
-    private final ConcurrentHashMap<WebSocketSession, String> usersSessions;
+    private final ConcurrentHashMap<WebSocketSession, String> sessionToUser;
+    private final ConcurrentHashMap<String, WebSocketSession> userToSession;
 
     /**
      * Creates a new WebSocketSessionManager
      */
     private WebSocketSessionManager() {
         documentsSessions = new ConcurrentHashMap<>();
-        usersSessions = new ConcurrentHashMap<>();
+        sessionToUser = new ConcurrentHashMap<>();
+        userToSession = new ConcurrentHashMap<>();
     }
 
     /**
@@ -48,11 +51,12 @@ public class WebSocketSessionManager {
      */
     public boolean addSession(WebSocketSession session, String documentId, String userId) {
         synchronized (WebSocketSessionManager.class) {
-            if (documentsSessions.containsKey(session) || usersSessions.containsKey(session)) {
+            if (documentsSessions.containsKey(session) || sessionToUser.containsKey(session)) {
                 return false;
             }
             documentsSessions.put(session, documentId);
-            usersSessions.put(session, userId);
+            sessionToUser.put(session, userId);
+            userToSession.put(userId, session);
 
             return true;
         }
@@ -66,11 +70,17 @@ public class WebSocketSessionManager {
      */
     public boolean removeSession(WebSocketSession session) {
         synchronized (WebSocketSessionManager.class) {
-            if (!documentsSessions.containsKey(session) || !usersSessions.containsKey(session)) {
+            if (!documentsSessions.containsKey(session) || !sessionToUser.containsKey(session)) {
                 return false;
             }
             documentsSessions.remove(session);
-            usersSessions.remove(session);
+
+            if (sessionToUser.containsKey(session)) {
+                String userId = sessionToUser.remove(session);
+                if (userId != null) {
+                    userToSession.remove(userId);
+                }
+            }
 
             return true;
         }
@@ -93,7 +103,30 @@ public class WebSocketSessionManager {
      * @return          The user id of the session, null if the session is not in the WebSocketSessionManager
      */
     public String getUserId(WebSocketSession session) {
-        return usersSessions.get(session);
+        return sessionToUser.get(session);
+    }
+
+    /**
+     * Returns the session of a user
+     *
+     * @param userId   The user id to get the session from
+     * @return          The session of the user, null if the user is not in the WebSocketSessionManager
+     */
+    public WebSocketSession getSession(String userId) {
+        return userToSession.get(userId);
+    }
+
+    /**
+     * Returns the document id of a user
+     *
+     * @param userId    The user id to get the document id from
+     * @return          The document id of the user, null if the user is not in the WebSocketSessionManager
+     */
+    public String getDocumentId(String userId) {
+        WebSocketSession session = getSession(userId);
+        if (session == null)    return null;
+
+        return getDocumentId(session);
     }
 
     /**
@@ -103,6 +136,6 @@ public class WebSocketSessionManager {
      * @return          True if the session is already connected, false otherwise
      */
     public boolean isAlreadyConnected(WebSocketSession session) {
-        return documentsSessions.containsKey(session) || usersSessions.containsKey(session);
+        return documentsSessions.containsKey(session) || sessionToUser.containsKey(session);
     }
 }
