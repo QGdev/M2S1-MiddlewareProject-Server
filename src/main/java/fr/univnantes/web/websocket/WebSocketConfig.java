86b8f9b28463acf0f8d1292b0d76422867db316e
Quentin GOMES DOS REIS <quentin.gomes.dev@gmail.com>
2023-11-12 14:39:10 +0100

    Added documentation in WebSocketConfig.java

--- src/main/java/fr/univnantes/web/websocket/WebSocketConfig.java
+++ src/main/java/fr/univnantes/web/websocket/WebSocketConfig.java
@@ -5,10 +5,21 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+/**
+ * Holds the websocket configuration.
+ * <p>
+ *     The websocket configuration is used to register the websocket handler.
+ *     It is used to handle the websocket requests.
+ * </p>
+ */
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    /**
+     * Registers the websocket handler.
+     * @param registry  The websocket handler registry
+     */
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new WebSocketHandler(), "/ws").setAllowedOrigins("*");
