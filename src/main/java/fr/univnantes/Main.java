b8c7b2a5df4180e4db811fa4f8a759ab05e93b1e
Quentin GOMES DOS REIS <quentin.gomes.dev@gmail.com>
2023-11-13 13:58:23 +0100

    Added line break insertion in Main.java

--- src/main/java/fr/univnantes/Main.java
+++ src/main/java/fr/univnantes/Main.java
@@ -24,82 +24,12 @@ public class Main {
             document.insert(0, 0, (char) (i + 97));
         }
 
-        /*
-        String testString = "Ceci est un test";
-        char[] charArray = testString.toCharArray();
-
-        for (int i = 5; i > 0; i--) {
-            for (int j = charArray.length - 1 ; j >= 0; j--) {
-            //for (int j = 0 ; j < charArray.length; j++) {
-                document.insert(i, j, charArray[j]);
-                System.out.println("i = " + i + " j = " + j + " char = " + charArray[j] + " document = " + document);
-            }
-        }*/
-
-
-        /*for (int c = 0; c < 5; c++) {
-            document.insert(0, 0, (char) (c + 97));
-        }*/
-
-        //  Insert 5 lines of 5 characters
-        // abcde
-        // fghij
-        // klmno
-        // pqrst
-        // uvwxy
-
-        for (int l = 0; l < 5; l++) {
-            for (int c = 0; c < 5; c++) {
-                document.insert(l + 10, c, (char) (l * 5 + c + 97));
-            }
-        }
-
-        // Remove one letter on two
-        // ace
-        // gik
-        // moq
-        // suw
-        // y
-
-        for (int l = 0; l < 5; l++) {
-            for (int c = 0; c < 5; c++) {
-                if ((l + c) % 2 == 0) {
-                    document.modify(10+l, c, ' ');
-                }
-            }
-        }
         System.out.println(document);
-
-        // Test deleteLineBreak
-        System.out.println(document.getLineCount());
-        document.deleteLineBreak(0);
-        System.out.println(document.getLineCount());
-        document.deleteLineBreak(0);
-        System.out.println(document.getLineCount());
-        document.deleteLineBreak(0);
-        System.out.println(document.getLineCount());
-        document.deleteLineBreak(0);
-        System.out.println(document.getLineCount());
-        document.deleteLineBreak(0);
-        System.out.println(document.getLineCount());
-
-
-
+        document.insertLineBreak(0, 0);
+        System.out.println("Insert line break");
         System.out.println(document);
-
-        String test = document.toString();
-        char[] test1 = test.toCharArray();
-
-        for (char c : test1) {
-            if (c != '\n') {
-                System.out.print((byte)(c));
-                System.out.print('|');
-            }
-            else {
-                System.out.println();
-            }
-        }
-        System.out.println(document.getLineCount());
-
+        document.deleteLineBreak(0);
+        System.out.println("Delete line break");
+        System.out.println(document);
     }
 }
