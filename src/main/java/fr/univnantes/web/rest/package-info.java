aa787b477f2c9539adc20f88f5834b1bf8c9298f
Quentin GOMES DOS REIS <quentin.gomes.dev@gmail.com>
2023-11-15 12:44:50 +0100

    Added package-info.java into web/rest package

---
+++ src/main/java/fr/univnantes/web/rest/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains all the REST controllers.
+ * <p>
+ *     This package includes the following classes:
+ *     <ul>
+ *         <li>{@link fr.univnantes.web.rest.RestApiController} - REST controller for documents.
+ *         <li>{@link fr.univnantes.web.rest.Utils} - Utility class for REST controllers.
+ *     </ul>
+ * </p>
+ */
+package fr.univnantes.web.rest;
