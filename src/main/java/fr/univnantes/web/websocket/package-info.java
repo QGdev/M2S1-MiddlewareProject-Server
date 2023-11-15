3be9540d6a42921a0f183e377ad35c480a917a35
Rod4401 <m.meunier.rodrigue@gmail.com>
2023-11-15 11:37:31 +0100

    Create package-info.java
    In fr.univnantes.web.websocket

---
+++ src/main/java/fr/univnantes/web/websocket/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Package containing all classes for the websockets.
+ *
+ * <p>
+ * This package includes the following packages:
+ *
+ * <ul>
+ * <li>{@link fr.univnantes.web.websocket.instruction} - Contains all instructions for encapsulate all informations to communicates.
+ * <li>{@link fr.univnantes.web.websocket.WebSocketConfig} - The class for the websockets configuration.
+ * <li>{@link fr.univnantes.web.websocket.WebSocketHandler} - The class for the websocket handler.
+ * <li>{@link fr.univnantes.web.websocket.WebSocketSessionManager} - The class representing the websockets manager.
+ * </ul>
+ */
+package fr.univnantes.web.websocket;
